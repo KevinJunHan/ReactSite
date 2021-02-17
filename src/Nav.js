12d472c804836a6c709d7ee37ebe0e82a53c1e47
KevinJunHan <KevinJunHan@outlook.com>
2021-02-17 21:10:26 +0800

    small update

--- src/Nav.js
+++ src/Nav.js
@@ -8,88 +8,83 @@ import contract from "./Images/contract.png";
 import { render } from "@testing-library/react";
 import { motion } from "framer-motion";
 
-function hello() {
-  return alert("Clicked");
-}
-
 class Nav extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { nam: "" };
-    this.toggleDesc = this.toggleDesc.bind(this);
-  }
+	constructor(props) {
+		super(props);
+		this.state = { nam: "" };
+		this.toggleDesc = this.toggleDesc.bind(this);
+	}
 
-  toggleDesc(props) {
-    const name = props;
-    this.setState({ nam: name }, () => console.log(this.state.nam));
-  }
+	toggleDesc(props) {
+		const name = props;
+		this.setState({ nam: name }, () => console.log(this.state.nam));
+	}
 
-  render() {
-    const navStyle = {
-      color: "grey",
-    };
+	render() {
+		const navStyle = {
+			color: "grey",
+		};
 
-    const nam = this.state.nam;
+		const nam = this.state.nam;
 
-    return (
-      <nav>
-        {/* <img className="logo" src={logo} alt="logo" /> */}
-        <ul className="nav-links">
-          <NavLink
-            className="abt"
-            onClick={() => this.toggleDesc("lol")}
-            style={navStyle}
-            to="/About"
-            activeClassName="selected"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "bisque",
-            }}
-          >
-            <li>
-              About_Me
-              <img className="user" src={user} alt="user" />
-            </li>
-          </NavLink>
-          <NavLink
-            className="abt"
-            style={navStyle}
-            to="/Portfolio"
-            activeClassName="selected"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "bisque",
-            }}
-          >
-            <li>
-              Portfolio
-              <img className="folder" src={folder} alt="folder" />
-            </li>
-          </NavLink>
+		return (
+			<nav>
+				{/* <img className="logo" src={logo} alt="logo" /> */}
+				<ul className="nav-links">
+					<NavLink
+						className="abt"
+						style={navStyle}
+						to="/About"
+						activeClassName="selected"
+						activeStyle={{
+							fontWeight: "bold",
+							color: "bisque",
+						}}
+					>
+						<li>
+							About_Me
+							<img className="user" src={user} alt="user" />
+						</li>
+					</NavLink>
+					<NavLink
+						className="abt"
+						style={navStyle}
+						to="/Portfolio"
+						activeClassName="selected"
+						activeStyle={{
+							fontWeight: "bold",
+							color: "bisque",
+						}}
+					>
+						<li>
+							Portfolio
+							<img className="folder" src={folder} alt="folder" />
+						</li>
+					</NavLink>
 
-          {/* <li>
+					{/* <li>
             Hire_Me
             <img className="contract" src={contract} alt="contract" />
           </li> */}
-          <NavLink
-            className="abt"
-            style={navStyle}
-            to="/HireMe"
-            activeClassName="selected"
-            activeStyle={{
-              fontWeight: "bold",
-              color: "bisque",
-            }}
-          >
-            <li>
-              Hire Me
-              <img className="contract" src={contract} alt="contract" />
-            </li>
-          </NavLink>
-        </ul>
-      </nav>
-    );
-  }
+					<NavLink
+						className="abt"
+						style={navStyle}
+						to="/HireMe"
+						activeClassName="selected"
+						activeStyle={{
+							fontWeight: "bold",
+							color: "bisque",
+						}}
+					>
+						<li>
+							Hire Me
+							<img className="contract" src={contract} alt="contract" />
+						</li>
+					</NavLink>
+				</ul>
+			</nav>
+		);
+	}
 }
 
 export default Nav;

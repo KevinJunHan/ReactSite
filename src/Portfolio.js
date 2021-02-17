79b57fea26103e73ac07bd2759c241a1cb3f64ce
KevinJunHan <KevinJunHan@outlook.com>
2021-02-17 19:30:17 +0800

    Added portfolio video

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -2,23 +2,48 @@ import React, { useState, useEffect } from "react";
 import reactDom from "react-dom";
 import "./App.css";
 import { useSpring, animated, interpolate } from "react-spring";
+import ReactPlayer from "react-player";
 
 function Portfolio() {
-  const props = useSpring({
-    to: [
-      { opacity: 1, color: "bisque" },
-      { opacity: 0.5, color: "brown" },
-    ],
-    from: { opacity: 1, color: "slategrey" },
-  });
+	const props = useSpring({
+		to: [
+			{ opacity: 1, color: "bisque" },
+			{ opacity: 0.5, color: "brown" },
+		],
+		from: { opacity: 1, color: "slategrey" },
+	});
 
-  return (
-    <div>
-      <animated.h1 style={props} className="header">
-        Porfolio
-      </animated.h1>
-    </div>
-  );
+	return (
+		<div>
+			<animated.h1 style={props} className="header">
+				Porfolio
+			</animated.h1>
+			<div className="players">
+				<div className="playerOnLeft">
+					<h1 className="genericText" style={{ color: "dodgerblue" }}>
+						Games developed:
+					</h1>
+					<ReactPlayer
+						width="400px"
+						height="300px"
+						muted="true"
+						playing="true"
+						url="www.youtube.com/watch?v=tqNft0iUPHY"
+					/>
+				</div>
+				<div className="playerOnRight">
+					<h1 className="genericText">FYP Mobile App:</h1>
+					<ReactPlayer
+						width="400px"
+						height="300px"
+						muted="true"
+						playing="true"
+						url="www.youtube.com/watch?v=FiFWPWR3mLs"
+					/>
+				</div>
+			</div>
+		</div>
+	);
 }
 
 export default Portfolio;
